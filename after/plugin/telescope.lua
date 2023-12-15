dabdc87291ecc6a4c1e47bdfe00986f126f47030
Simon Rodrigues <simon.rodrigues@spotme.com>
2023-12-15 08:11:34 +0100

    merge

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -16,10 +16,6 @@ telescope.setup({
 telescope.load_extension('fzf')
 
 local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
 
 local wk = require('which-key')
 wk.register({
@@ -27,9 +23,9 @@ wk.register({
 		name = '[s]earch',
 		f = { builtin.find_files, 'Find Files' },
 		g = { builtin.live_grep, 'Live Grep' },
-		b = { builtin.buffers, 'Buffers' },
 		h = { builtin.help_tags, 'Help Tags' }
-	}
+	},
+	['<Space>'] = { builtin.buffers, 'Buffer List' }
 }, {
 	prefix = '<leader>'
 })
